10bab7acaf0d5e3c5e7ab6563a32e8c0933f76d7
Syhotsky <39332361+Syhotsky@users.noreply.github.com>
2018-06-05 11:24:18 +0300

    Syhotsky

---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer(function(request, respons) {
+   console.log(request.method, request.url);
+   if (request.url == '/style.css') {
+       const css = fs.readFileSync('style.css', 'utf8');
+    respons.end(css);} else {
+
+       const html = fs.readFileSync('index.html', 'utf8');
+       respons.end(html);
+   }
+});
+
+server.listen('3000');
+console.log('Server started');
